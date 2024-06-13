50fcb4ddb26ecde8e555c9ed9efe14c837dc807e
viettungvuong <viettungvuong@gmail.com>
2024-06-13 10:47:04 +0700

    Thêm quản lý tiền

--- Assets/Scripts/Player/PlayerUnit.cs
+++ Assets/Scripts/Player/PlayerUnit.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerUnit : Unit
 {
     public int maxMoney;
+    public TextMeshProUGUI coinText;
     [HideInInspector] public int currentMoney;
     public override void Awake()
     {
         base.Awake();
         currentMoney = maxMoney;
+    }
+
+    private void LateUpdate() {
+        coinText.text = currentMoney.ToString();
     }
 }
