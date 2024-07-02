13909ca06184a212cf43b182e08bada01e219259
hongnhut2404 <nhutth2404@gmail.com>
2024-07-02 16:22:57 +0700

    Complete load menu with raw data

--- Assets/Scripts/WelcomeMenuButton/LoadBrowser.cs
+++ Assets/Scripts/WelcomeMenuButton/LoadBrowser.cs
@@ -5,10 +5,38 @@ using UnityEngine.UI;
 
 public class LoadBrowser : MonoBehaviour
 {
-    public GameObject buttpnPrefab;
+    public GameObject buttonPrefab;
     public GameObject buttonParent;
+
     private void OnEnable()
     {
-        
+        List<string> games;
+        if (GameLoading.LoadGames() == null)
+        {
+            games = new List<string>(new string[] { "EMPTY 1", "EMPTY 2", "EMPTY 3", "EMPTY 4", "EMPTY 5", "EMPTY 6" });
+        }
+        else
+        {
+            games = GameLoading.LoadGames();
+        }
+
+
+        Debug.Log("Number of games loaded: " + games.Count);
+
+
+        for (int i = 0; i < games.Count; i++)
+        {
+            GameObject newButton = Instantiate(buttonPrefab, buttonParent.transform);
+            LoadButton loadButtonComponent = newButton.GetComponent<LoadButton>();
+            loadButtonComponent.loadText.text = games[i];
+            int index = i; // Capture the current value of i
+            newButton.GetComponent<Button>().onClick.AddListener(() => SelectLoadFile(games[index]));
+        }
+
+    }
+
+    private void SelectLoadFile(string name)
+    {
+        Debug.Log("Load file name: " + name);
     }
 }
