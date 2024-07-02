976f6fb457e7b13aa091ed16864fafe1df0747ce
hongnhut2404 <nhutth2404@gmail.com>
2024-07-02 14:39:22 +0700

    initliaze load scene

---
+++ Assets/Scripts/WelcomeMenuButton/LoadBrowser.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadBrowser : MonoBehaviour
+{
+    public GameObject buttpnPrefab;
+    public GameObject buttonParent;
+    private void OnEnable()
+    {
+        
+    }
+}
