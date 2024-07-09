8975846bc2c1c8e5da119e7de336a17625e812fa
viettungvuong <viettungvuong@gmail.com>
2024-07-10 00:13:08 +0700

    mode ui

--- Assets/Scripts/UI/ModeUI.cs
+++ Assets/Scripts/UI/ModeUI.cs
@@ -6,8 +6,32 @@ public class ModeUI : MonoBehaviour
 {
     public GameObject fence, health, enemy;
 
+    void Awake()
+    {
+        if (PlayerUnit.playerMode == PlayerMode.SURVIVAL){
+            fence.SetActive(true);
+            health.SetActive(true);
+            enemy.SetActive(true);
+        }
+        else{
+            fence.SetActive(false);
+            health.SetActive(false);
+            enemy.SetActive(false);
+        }
+    }
+
     void Start()
     {
+        if (PlayerUnit.playerMode == PlayerMode.SURVIVAL){
+            fence.SetActive(true);
+            health.SetActive(true);
+            enemy.SetActive(true);
+        }
+        else{
+            fence.SetActive(false);
+            health.SetActive(false);
+            enemy.SetActive(false);
+        }
     }
 
     // Update is called once per frame
